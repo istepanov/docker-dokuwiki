44011606b9dce08b7e32d622bc0ada107b92ded3
Nicolas De los Santos <ndls05@gmail.com>
2016-07-26 22:24:04 +0200

    Modified to allow storage volume mounted on host

--- start.sh
+++ start.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+if ! [ -e /var/dokuwiki-storage/data ]; then
+    mkdir -p /var/www /var/dokuwiki-storage/data
+fi
+
+for dir in data/pages data/meta data/media data/media_attic data/media_meta data/attic conf; do
+    if ! [ -e /var/dokuwiki-storage/$dir ]; then
+        cp -r /var/www/$dir /var/dokuwiki-storage/$dir
+    fi
+    rm -rf /var/www/$dir
+    ln -s /var/dokuwiki-storage/$dir /var/www/$dir
+done
+
 chown -R www-data:www-data /var/www
 chown -R www-data:www-data /var/dokuwiki-storage
 
